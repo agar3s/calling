05cd39880d196143d02ee7726fd9a462e2ba14a4
Giovanny Beltran <ekeisco@gmail.com>
2018-03-06 14:16:22 -0500

    adds new assets, changes movement model to custom one

--- src/scenes/boot.js
+++ src/scenes/boot.js
@@ -2,20 +2,27 @@ import Player from '../sprites/player'
 import Control from '../util/control'
 
 let basicMap = 
-`-16,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,16
--16,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,16
--16,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2,0,0,45,16
--16,0,0,0,0,0,0,0,0,0,26,26,26,3,47,3,4,26,53,16
--16,0,0,0,45,0,0,0,0,8,9,9,9,-11,55,11,9,9,9,10
--16,0,0,0,53,0,0,46,23,16,0,0,0,-19,55,19,0,0,0,0
--16,0,0,0,22,14,14,54,15,16,0,0,0,-27,55,27,0,0,0,0
--16,0,0,0,30,0,0,54,0,16,0,0,-19,63,55,63,20,20,20,34
--16,0,0,14,14,22,0,54,0,16,0,0,-19,0,55,0,0,0,0,42
--16,0,0,0,0,30,0,54,2,16,0,0,-19,0,55,0,0,0,0,42
--16,0,0,0,26,30,1,54,23,16,0,0,-13,12,12,12,12,13,0,42
--10,0,14,15,8,9,9,9,9,10,0,0,0,0,0,0,0,0,0,42
--0,0,0,0,16,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
-9,9,9,9,10,0,0,0,0,0,0,0,11,12,12,12,12,12,12,12`
+`8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8
+8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8
+8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8
+8,8,8,8,8,8,0,1,1,2,8,0,1,1,2,8,8,8,8,8
+8,8,8,8,8,8,0,6,6,2,8,0,6,6,2,8,8,8,8,8
+8,8,8,8,3,4,0,6,6,2,8,0,6,6,2,8,8,8,8,8
+8,8,8,8,8,9,0,6,6,2,8,0,6,6,2,8,8,8,8,8
+1,1,1,1,1,1,5,6,6,2,8,0,6,6,7,1,1,1,1,1
+8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8
+8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8
+8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8
+1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,2,8,8,8,0
+8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,0,6
+8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,0,6,6
+8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,0,6,6,6
+8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,0,6,6,6,6
+1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,5,6,6,6,6
+6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6
+6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6
+6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6
+6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6`
 
 const ORDER_CODES = {
   JUMP: 1,
@@ -42,24 +49,20 @@ class BootScene extends Phaser.Scene {
   }
 
   preload() {
-    this.load.spritesheet('tiles', '../assets/cavesofgallet_tiles_t.png', {frameWidth: 8, frameHeight: 8})
+    this.load.spritesheet('characters', '../assets/characters.png', {frameWidth: 16, frameHeight: 16})
+    this.load.spritesheet('platforms', '../assets/platforms.png', {frameWidth: 16, frameHeight: 16})
+    this.load.spritesheet('ui', '../assets/ui.png', {frameWidth: 32, frameHeight: 32})
   }
 
   create() {
-    let scale = 3
+    let scale = 2
     let padding = 1
-    let ts = 8 // tileSize
-    for (var j = 0; j < 12; j++) {
-      for (var i = 0; i < 8; i++) {
-        let image = this.add.tileSprite(50 + i*(ts*scale+padding), 50 + j*(ts*scale+padding), ts, ts, 'tiles', j*8+i)
-        image.setScale(scale, scale)
-      }
-    }
+    let ts = 16 // tileSize
     
     let platforms = this.physics.add.staticGroup()
 
-    let xOffset = 300
-    let yOffset = 100
+    let xOffset = ts*scale
+    let yOffset = ts*scale
     
     padding = 0
     let map = basicMap.split('\n').map(row => row.split(',').map(tile => parseInt(tile)))
@@ -70,10 +73,10 @@ class BootScene extends Phaser.Scene {
         let x = xOffset + i * (ts * scale + padding)
         let tile = row[i]
         let invert = (tile < 0)?-1:1
-        if ([8, 9, 10, 11, 12, 13, 14, 15, 16, 18, 19, 20, 22, 24].indexOf(invert*tile)!=-1) {
-          platforms.create(x, y, 'tiles', tile * invert).setScale(scale, scale).refreshBody().setScale(invert * scale, scale)
+        if ([0, 1, 2, 3].indexOf(invert*tile)!=-1) {
+          platforms.create(x, y, 'platforms', tile * invert).setScale(scale, scale).refreshBody().setScale(invert * scale, scale)
         } else {
-          this.add.tileSprite(x, y, ts, ts, 'tiles', tile * invert).setScale(invert * scale, scale)
+          this.add.tileSprite(x, y, ts, ts, 'platforms', tile * invert).setScale(invert * scale, scale)
         }
       }
     }
@@ -81,29 +84,29 @@ class BootScene extends Phaser.Scene {
 
     this.anims.create({
       key: 'flying-blue',
-      frames: [{key: 'tiles', frame: 41}, {key: 'tiles', frame: 49}],
+      frames: [{key: 'characters', frame: 2}, {key: 'characters', frame: 3}],
       repeat: -1,
       frameRate: 3
     })
 
     this.anims.create({
       key: 'flying-red',
-      frames: [{key: 'tiles', frame: 31}, {key: 'tiles', frame: 38}],
+      frames: [{key: 'characters', frame: 0}, {key: 'characters', frame: 1}],
       repeat: -1,
       frameRate: 6
     })
 
     this.anims.create({
       key: 'idle-red',
-      frames: [{key: 'tiles', frame: 39}]
+      frames: [{key: 'characters', frame: 0}]
     })
 
-    this.add.sprite(xOffset + 8*scale*5+4, yOffset, 'tiles').setScale(scale).play('flying-blue')
+    this.add.sprite(xOffset + 8*scale*5+4, yOffset, 'characters').setScale(scale).play('flying-blue')
 
     this.player = new Player({
       scene: this,
-      x: xOffset + 4 + (8 * scale * 6),
-      y: yOffset + 4 + (8 * scale * 4)
+      x: xOffset + 16 + (16 * scale * 6),
+      y: yOffset + 16 + (16 * scale * 2)
     })
 
     this.physics.add.collider(this.player.sprite, platforms)
@@ -117,41 +120,46 @@ class BootScene extends Phaser.Scene {
     this.status = STATUS.WAITING
     this.turnTransition = 0
     this.order = 0
+
+
   }
+
 
   update (time, dt) {
     this.turnTransition -= dt
 
     if (this.status === STATUS.WAITING) {
+      let TIME_TO_ANIMATE = 300
       // read keys
       if (this.control.isUp()) {
         this.order = ORDER_CODES.JUMP
-        this.player.jump()
+        this.player.jump(TIME_TO_ANIMATE)
       } else if (this.control.isLeft()) {
         this.order = ORDER_CODES.LEFT
-        this.player.turnLeft()
+        this.player.turnLeft(TIME_TO_ANIMATE)
       } else if (this.control.isRight()) {
         this.order = ORDER_CODES.RIGHT
-        this.player.turnRight()
+        this.player.turnRight(TIME_TO_ANIMATE)
       } else if (this.control.isDown()) {
         this.order = ORDER_CODES.DOWN
+        this.player.down(TIME_TO_ANIMATE)
       } else if (this.control.isTalk()) {
         this.order = ORDER_CODES.TALK
       } else if (this.control.isAttack()) {
         this.order = ORDER_CODES.ATTACK
       } else if (this.control.isJumpLeft()) {
         this.order = ORDER_CODES.JUMP_LEFT
-        this.player.jump()
-        this.player.turnLeft()
+        this.player.jump(TIME_TO_ANIMATE)
+        this.player.turnLeft(TIME_TO_ANIMATE)
       } else if (this.control.isJumpRight()) {
         this.order = ORDER_CODES.JUMP_RIGHT
-        this.player.jump()
-        this.player.turnRight()
+        this.player.jump(TIME_TO_ANIMATE)
+        this.player.turnRight(TIME_TO_ANIMATE)
       }
 
       if (this.order > 0) {
         // take decisions
-        this.player.enableTime(1)
+        this.player.enableTime(TIME_TO_ANIMATE, 1)
         this.turnTransition = 300
         this.status = STATUS.TRANSITION
       }
@@ -159,13 +167,15 @@ class BootScene extends Phaser.Scene {
     }
 
     if (this.status === STATUS.TRANSITION) {
-      this.player.update()
 
       if (this.turnTransition < 0) {
+        this.player.update()
         this.turnTransition = 150
         this.player.disableTime()
         this.order = 0
         this.status = STATUS.DELAY
+      } else {
+        this.player.update(dt)
       }
       return
     }
@@ -175,8 +185,8 @@ class BootScene extends Phaser.Scene {
         this.status = STATUS.WAITING
       }
     }
-
   }
+
 }
 
 export default BootScene
--- src/sprites/player.js
+++ src/sprites/player.js
@@ -1,46 +1,97 @@
-
+const SCALE = 2
+const WIDTH = 16
 export default class Player {
   constructor (config) {
     let scene = config.scene
     let x = config.x
     let y = config.y
-    this.sprite = scene.physics.add.sprite(x, y, 'tiles')
-    this.sprite.setScale(3 * 0.7).play('idle-red')
+    this.sprite = scene.add.sprite(x-WIDTH*SCALE*0.5, y-WIDTH*SCALE*0.5, 'characters')
+    this.sprite.setOrigin(0.5, 0.5)
+    this.sprite.setScale(SCALE).play('idle-red')
     this.speed = {x: 0, y: 0}
+    this.acceleration = {x: 0, y: 0}
+    this.fixPositionToGrid()
+
+    this.grid = config.scene.add.graphics(0,0)
+    this.grid.lineStyle(0x220022, 1)
+    this.grid.fillStyle(0x220022, 0.2)
+    this.grid.beginPath()
+    for (var j = 0; j < 50; j++) {
+      for (var i = 0; i < 50; i++) {
+        this.grid.fillRect(i*SCALE*WIDTH+1 + SCALE*WIDTH/2, j*SCALE*WIDTH+1 + SCALE*WIDTH/2, SCALE*WIDTH-2, SCALE*WIDTH-2)
+      }
+    }
+    this.grid.closePath()
+    this.timeFromTransition = 0
+    this.fixedTimeForTransition = 0 
   }
 
-  update () {
-    this.sprite.setVelocityX(this.speed.x)
+  update (dt) {
+    if(!dt) {
+      dt = this.fixedTimeForTransition - this.timeFromTransition
+    }
+    this.timeFromTransition += dt
+    this.speed.y += this.acceleration.y * dt
+    this.sprite.x +=  this.speed.x*dt
+    this.sprite.y +=  this.speed.y*dt
   }
 
-  jump () {
-    this.sprite.setVelocityY(-300)
+  jump (transitionTime) {
+    // vy = (h2 - h1) / t
+    let desiredHigh = 1
+    let gravityDistance = 2
+    let yDistance = (-desiredHigh - gravityDistance)*WIDTH*SCALE
+    this.speed.y = yDistance/transitionTime
   }
 
-  turnLeft () {
-    this.sprite.setScale((3 * 0.7), 3 * 0.7)
-    this.speed.x = -80
+  down (transitionTime) {
+    //this.speed.y = WIDTH*SCALE/transitionTime
+    this.speed.y = 0
+    
+    console.log(this.acceleration.y)
   }
 
-  turnRight () {
-    this.sprite.setScale(-1*(3 * 0.7), 3 * 0.7)
-    this.speed.x = 80
+  turnLeft (transitionTime) {
+    this.sprite.setScale(-1*(SCALE), SCALE)
+    this.speed.x = -WIDTH*SCALE/transitionTime
   }
 
-  enableTime (factor) {
-    this.sprite.setAccelerationY(1000 * factor)
+  turnRight (transitionTime) {
+    this.sprite.setScale((SCALE), SCALE)
+    this.speed.x = WIDTH*SCALE/transitionTime
+  }
+
+  enableTime (transitionTime, factor) {
+    this.acceleration.y = WIDTH*SCALE*3/(transitionTime*transitionTime)
+    this.fixPositionToGrid()
     this.sprite.anims.play('flying-red', true)
+    this.timeFromTransition = 0
+    this.fixedTimeForTransition = transitionTime
+  }
+
+  fixPositionToGrid () {
+    let ts = WIDTH*SCALE
+    let xIndex = ~~(this.sprite.x/ts)
+    let yIndex = ~~(this.sprite.y/ts)
+    let x = (this.sprite.x - (xIndex*ts)) > (ts/2)? (xIndex + 1): xIndex
+    let y = (this.sprite.y - (yIndex*ts)) > (ts/2)? (yIndex + 1): yIndex
+    this.sprite.x = x*ts
+    this.sprite.y = y*ts
   }
 
   disableTime () {
-    if (this.sprite.body.velocity.y === 0) {
-      this.sprite.anims.play('idle-red')
-    }
+   // if (this.sprite.body.velocity.y === 0) {
+   //   this.sprite.anims.play('idle-red')
+   // }
+    console.log('pre', this.sprite.y)
+    console.log('speed y', this.speed.y)
+    console.log('totalTime', this.timeFromTransition)
+    this.fixPositionToGrid()
+    console.log('pos', this.sprite.y)
     this.speed.x = 0
     this.speed.y = 0
-    this.sprite.setVelocityX(0)
-    this.sprite.setVelocityY(0)
-    this.sprite.setAccelerationY(0)        
+    this.acceleration.y = 0
+    this.sprite.update()
   }
 
 
